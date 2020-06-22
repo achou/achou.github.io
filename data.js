384adc4ee10aa4a8626df4b8e1dffb8b7cf2cfc6
Ashwani Chandra <achou@Ashwanis-MacBook-Pro.local>
2020-06-23 00:04:56 +0530

    adding jump to page

--- data.js
+++ data.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
 			$('.prev').prop('disabled', false);	
 			$('.next').prop('disabled', false);	
 		}
+		$("#card_number").val("");
 	})
 	$(".next").click(function(){
 		if(counter < 435) {
@@ -32,7 +33,6 @@ $(document).ready(function() {
 			$tile.find(".front").css("background-image", "url('assets/Card " + counter + " Front.jpg')");
 			$tile.find(".back").css("background-image", "url('assets/Card " + counter + " Back.jpg')");
 			$("#card").flip(false);
-			console.log("counter is" + counter)
 			$(".card-no").text("Card # " + counter);
 		}
 		if (counter == 435) {
@@ -43,7 +43,34 @@ $(document).ready(function() {
 			$('.prev').prop('disabled', false);	
 			$('.next').prop('disabled', false);	
 		}
+		$("#card_number").val("");
 	})
+
+	$("#card_number").change(function(){
+	  data_counter = parseInt($("#card_number").val());
+	  if(data_counter > 0 && data_counter < 436) {
+	  	counter = data_counter;
+		$tile.find(".front").css("background-image", "url('assets/Card " + counter + " Front.jpg')");
+		$tile.find(".back").css("background-image", "url('assets/Card " + counter + " Back.jpg')");
+		$("#card").flip(false);
+		$(".card-no").text("Card # " + counter);
+		if (counter == 1) {
+			$('.prev').prop('disabled', true);
+			$('.next').prop('disabled', false);	
+		}
+		else if (counter == 435) {
+			$('.next').prop('disabled', true);
+			$('.prev').prop('disabled', false);	
+		}
+		else {
+			$('.prev').prop('disabled', false);	
+			$('.next').prop('disabled', false);	
+		}
+	  }
+	  else {
+	  	$("#card_number").val(counter);
+	  }
+	});
 
 });
 
